4a2fd5550685f98077309bfe69ddc2493cb6302c
met <facebook4004@hotmail.com>
2021-10-13 17:11:32 -0400

    stuff

--- albania/pages/models.py
+++ albania/pages/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Page(models.Model):
-    name = models.CharField(max_length=200, name=True)
-    username = models.CharField(max_length=200, name=True)
-
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
     def __str__(self):
         return self.name
--- albania/pages/views.py
+++ albania/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def home(request):
+    return render(request, 'pages/home.html' )
